2b2b4aada5c246f3f986fb1fb67913bdec211b6b
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2021-05-20 21:32:19 +0300

    Footer change

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -1,38 +1,28 @@
-import React from 'react';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+import React from "react";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
 
-const FooterNav = ({
-  className,
-  ...props
-}) => {
-
-  const classes = classNames(
-    'footer-nav',
-    className
-  );
+const FooterNav = ({ className, ...props }) => {
+  const classes = classNames("footer-nav", className);
 
   return (
-    <nav
-      {...props}
-      className={classes}
-    >
+    <nav {...props} className={classes}>
       <ul className="list-reset">
         <li>
-          <Link to="#0">Contact</Link>
+          <Link to="#0">İletişim</Link>
         </li>
         <li>
-          <Link to="#0">About us</Link>
+          <Link to="#0">Bize Ulaşın</Link>
         </li>
         <li>
-          <Link to="#0">FAQ's</Link>
+          <Link to="#0">Sık Sorulan Sorular</Link>
         </li>
         <li>
-          <Link to="#0">Support</Link>
+          <Link to="#0">Destek</Link>
         </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default FooterNav;
